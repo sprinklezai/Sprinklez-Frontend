6695ce8a4c9162e945d3d931385dbb74bbaeba43
sprinklezai <sprinklezai@gmail.com>
2026-07-09 20:36:31 +0400

    v2 changes for google drive access

--- backend/services/googleDriveSalesService.js
+++ backend/services/googleDriveSalesService.js
@@ -21,28 +21,31 @@ function getConfig() {
   return { clientEmail, privateKey, folderId };
 }
 
-function getDriveClient() {
+async function getDriveClient() {
   const { clientEmail, privateKey } = getConfig();
 
-  const auth = new google.auth.JWT(
-    clientEmail,
-    null,
-    privateKey,
-    ["https://www.googleapis.com/auth/drive.readonly"]
-  );
+  const auth = new google.auth.JWT({
+    email: clientEmail,
+    key: privateKey,
+    scopes: ["https://www.googleapis.com/auth/drive.readonly"],
+  });
+
+  await auth.authorize();
 
   return google.drive({ version: "v3", auth });
 }
 
 async function findSalesZipFile(month = "2026_06") {
   const { folderId } = getConfig();
-  const drive = getDriveClient();
+  const drive = await getDriveClient();
 
   const fileName = `${month}_sales.zip`;
 
   const response = await drive.files.list({
     q: `'${folderId}' in parents and name='${fileName}' and trashed=false`,
     fields: "files(id, name, size, modifiedTime)",
+    supportsAllDrives: true,
+    includeItemsFromAllDrives: true,
   });
 
   const file = response.data.files?.[0];
@@ -55,13 +58,14 @@ async function findSalesZipFile(month = "2026_06") {
 }
 
 async function downloadSalesZipFromDrive(month = "2026_06") {
-  const drive = getDriveClient();
+  const drive = await getDriveClient();
   const file = await findSalesZipFile(month);
 
   const response = await drive.files.get(
     {
       fileId: file.id,
       alt: "media",
+      supportsAllDrives: true,
     },
     {
       responseType: "arraybuffer",
